1ab8736348d048eedc86786815acae8a1de32ea5
jon-grangien <jonathan.grangien@gmail.com>
2022-03-22 16:46:13 +0100

    Implement add book mutation

--- Api/Data/Mutation.cs
+++ Api/Data/Mutation.cs
@@ -10,6 +10,20 @@ public class Mutation
 		return book;
 	}
 
+	public Book AddBook(string title, string? description, [ID] int authorId, BookriofyDbContext dbContext)
+	{
+		var author = dbContext.Authors.FirstOrDefault(a => a.Id == authorId);
+		if (author == null)
+		{
+			throw new Exception("Author not found");
+		}
+
+		var book = new Book() { Title = title, Description = description, Author = author };
+		dbContext.Books.Add(book);
+		dbContext.SaveChanges();
+		return book;
+	}
+
 	public Author AddAuthor(string name, string bio, BookriofyDbContext dbContext)
 	{
 		var author = new Author { Name = name, Bio = bio };
